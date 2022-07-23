e46445323db33bb67382e33691c211001a713f1e
Xavier_Marquez <xavier.ray.marquez@gmail.com>
2022-07-23 14:01:34 -0700

    fixing styling

--- my-react-app/src/Components/pages/Home.js
+++ my-react-app/src/Components/pages/Home.js
@@ -1,10 +1,31 @@
 import React from "react";
+// import "../../styles/Header.css";
+// import Header from "../Header";
+
+const styles = {
+  Header: {
+    background: "blue",
+    display: "flex",
+    flexDirection: "column",
+  },
+
+  h1: {
+    textAlign: " center",
+    color: "black",
+    background: "blue",
+  },
+
+  div: {
+    textAlign: " center",
+    color: "black",
+  },
+};
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
+    <div style={styles.Header}>
+      <h1 style={styles.h1}>Home Page</h1>
+      <p style={styles.div}>
         Thank you for visiting my react website look around and leave me a
         message.
       </p>
